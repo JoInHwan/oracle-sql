dd31d1609b3122dbc8173eca4e43adf227310ae2
JoInHwan <dlsghks135@naver.com>
2024-04-18 10:15:55 +0900

    dept&emp 테이블용 VO 생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,12 @@
+package vo;
+
+// VO(Value Object) 범위 안 DTO(Data Transfer Object) , Domain
+public class Dept {
+	public int deptNo;
+	public String dname;
+	public String loc;	
+	
+	
+	
+	
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,12 @@
+package vo;
+
+public class Emp {
+	public int Empno;
+	public String Ename;
+	public String job;
+	public int Mgr;
+	public String Hiredate;
+	public double Sal;
+	public double Comm;
+	public int Deptno;
+}
